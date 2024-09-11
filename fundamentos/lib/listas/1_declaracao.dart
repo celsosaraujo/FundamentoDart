56bc787443e3ea032c5213279c14e6661f11ee04
Celso Serrano Araujo <celso.sraujo@hotmail.comm>
2024-09-10 22:21:47 -0300

    Listas

---
+++ fundamentos/lib/listas/1_declaracao.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+
+  // Declaração e inicialização de uma lista de inteiros
+  // Tipos Genéricos
+  List<int> listaNumeros = [1,2,3];
+
+  List<String> listaTextos = ['Fulano','Beltrano','Sicrano'];
+
+  // Declaração por inferência e atribuição
+  var listaNumerosInferencia = [1,2,3];
+
+  var listaTextosInferencia = ['Fulano','Beltrano','Sicrano'];
+
+  //declaração de lista vazia
+  
+  List<int> listaSemDadosGenerico = [];
+
+  var listaSemDadosInferencia = [];
+
+  var listaSemDadosInferenciaGenerico = <int>[];
+
+  var listaSemDadosInferenciaGenericoString = <String>[];
+
+}
