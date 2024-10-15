2a42f638c089bea120297796041e00335eb4d5e2
Celso Serrano Araujo <celso.sraujo@hotmail.comm>
2024-10-15 20:11:47 -0300

    Tratamento de Exceçoes

--- fundamentos/lib/tratamento_excecao/divisao_por_zero.dart
+++ fundamentos/lib/tratamento_excecao/divisao_por_zero.dart
@@ -2,7 +2,12 @@ void main() {
   try {
     int resultado = divideNumero(10, 0);
     print("Resultado da Divisão $resultado");
-  } catch (e) {
+  // } on IntegerDivisionByZeroException{
+  }/*on UnsupportedError{
+    print("O divisor deve ser diferente de zero ");
+  }on NoSuchMethodError{
+    print("O dividendo e o divisor deve ser um número");
+  } */catch(e) {
     print("Erro $e");
   } finally {
     print("Fim do Sistema");
@@ -10,5 +15,13 @@ void main() {
 }
 
 int divideNumero(dividendo, divisor) {
-  return dividendo ~/ divisor;
+  // if(dividendo is! int || divisor is! int )
+  if(dividendo is! int )
+    throw Exception("O dividendo deve ser um número");
+  else if(divisor is! int )
+    throw Exception("O divisor deve ser um número");
+  else if(divisor == 0)  
+    throw Exception("O divisor deve ser diferente de zero");
+  else  
+    return dividendo ~/ divisor;
 }
