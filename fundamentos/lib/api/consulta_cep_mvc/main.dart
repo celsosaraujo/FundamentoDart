44c2cfb2a3e70e3dc9551853f94fc86e6ce597f4
Celso Serrano Araujo <celso.sraujo@hotmail.comm>
2024-11-04 22:30:24 -0300

    mvc

--- fundamentos/lib/api/consulta_cep_mvc/main.dart
+++ fundamentos/lib/api/consulta_cep_mvc/main.dart
@@ -1,10 +1,31 @@
+import 'dart:io';
 import 'endereco.dart';
 import 'endereco_controller.dart';
 
+String validaCEP(String? cep) {
+
+  if (cep == null || cep.isEmpty){
+    throw Exception("CEP inválido. Tente novamente!");
+  }else {
+    cep = cep.replaceAll(RegExp(r'[^0-9]'), '');
+    if (cep.length != 8) {
+      throw Exception("CEP inválido, deve possuir 8 números");
+    } else {
+      return cep;
+    }
+  }
+  
+}
+
 void main(List<String> args) async {
   final controller = EnderecoController();
+
+  print("Informe o CEP (Format 00000-000): ");
+  String? cep = stdin.readLineSync();
+
+  // if (cep != null && cep.isNotEmpty) {
   try {
-    Endereco endereco = await controller.buscarEndereco("17500005");
+    Endereco endereco = await controller.buscarEndereco(validaCEP(cep));
     print("Logradouro: ${endereco.logradouro}");
     print("Bairro: ${endereco.bairro}");
     print("Cidade: ${endereco.localidade}");
@@ -12,4 +33,7 @@ void main(List<String> args) async {
   } catch (e) {
     print(e);
   }
+  // }else{
+  //   print("CEP inválido. Tente novamente!");
+  // }
 }
